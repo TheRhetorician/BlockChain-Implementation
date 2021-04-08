7952d2a21b43fbf126e85247c5be0f0b3f529ab0
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-04-08 17:25:45 +0530

    Saving BlockChain using pickle in BlockChain.txt

--- Utils.py
+++ Utils.py
@@ -1,9 +1,11 @@
 import datetime
 import pickle
+import struct
 import hashlib
 import json
 import socket
 from threading import Thread
+import os
 
 class Block:
     def __init__(self, data, prevHash=''):
@@ -28,7 +30,7 @@ class Users:
         return
 
     def verifyTransaction(self):
-        return None
+        return 
 
     def verifyBlockChain(self):
         return
@@ -36,7 +38,24 @@ class Users:
 class Admin:
     def __init__(self):
         print("Admin Initiated")
-        self.sock = self.create_socket(('localhost', 5000))
+        sock = self.create_socket(('localhost', 5000))
+        Thread(target=self.start_threads, args=(sock,)).start()
+        if os.stat("BlockChain.txt").st_size == 0:
+            f = open('BlockChain.txt', 'wb')
+            block = Block("Genesis", '0')
+            pickle.dump([block], f)
+            f.close()
+
+
+    def createUser(self, username, password):
+        print("Inside createUser")
+        user = Users(username, password)
+        print(user.username, user.password)
+        # f = open('users.txt', 'rb')
+        # users = pickle.load(f)
+        # print(users)
+        # f.close()
+        return user
 
     def create_socket(self, address):
         listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -50,12 +69,13 @@ class Admin:
         while True:
             sock, address = listener.accept()
             print('Accepted connection from {}'.format(address))
-            handle_conversation(sock,address)
+            self.handle_conversation(sock,address)
 
     def handle_conversation(self, sock, address):
         try:
             while True:
-                handle_request(sock)
+                sock.sendall('Start Proof of work')
+                self.handle_request(sock)
         except EOFError:
             print('Client socket to {} has closed'.format(address))
         except Exception as e:
@@ -64,9 +84,19 @@ class Admin:
             sock.close()
 
     def handle_request(self, sock):
-        task = recv_untill(sock,b'?')
-        ans = get_ans(task)
-        sock.sendall(ans)
+        data = b''
+        payload_size = struct.calcsize("L")
+        print("Expecting Data")
+        while len(data) < payload_size:
+            data += sock.recv(4096)
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("L", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += sock.recv(4096)
+        block_data = data[:msg_size]
+        data = data[msg_size:]
+        block = pickle.loads(block_data)
 
     def recv_untill(self, sock, suffix):
         msg = sock.recv(4096)
@@ -85,9 +115,11 @@ class Admin:
         return
 
     def start_threads(self, listener, workers=4):
+        print("here")
         t = (listener,)
         for i in range(workers):
-            Thread(target=accept_forever, args=t).start()
+            Thread(target=self.accept_forever, args=t).start()
+        return
 
     
 
@@ -97,3 +129,11 @@ class Admin:
 # result = json.dumps(val)
 # print(result)
 # bl = Block(result)
+ad = Admin()
+print("After initialisation")
+ad.createUser("Hardik", "Hello")
+print("After create User")
+f = open("BlockChain.txt", "rb")
+block = pickle.load(f)
+print(block[0].data, block[0].timestamp, block[0].Hash)
+f.close()
